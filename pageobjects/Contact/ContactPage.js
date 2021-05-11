d457678409fd86ecdf60aebd35c144275a70b619
Yevheniia Poltorak <70563503+Tetravi@users.noreply.github.com>
2021-05-11 19:23:36 +0300

    Update ContactPage.js

--- pageobjects/Contact/ContactPage.js
+++ pageobjects/Contact/ContactPage.js
@@ -5,7 +5,7 @@ let BasePage = require('./../BasePage');
 class ContactPage extends BasePage {
     constructor() {
         super();
-		this.appNameHeader += '/about/who-we-are/contact';
+		this.appNameHeader += 'about/who-we-are/contact';
 		this.number = element(by.partialLinkText('tel:+12677599000'));
     }
 
