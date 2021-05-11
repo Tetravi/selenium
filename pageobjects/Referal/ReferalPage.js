d5441a2765fdc8e951cea60eb3f4a4298089aa55
Yevheniia Poltorak <70563503+Tetravi@users.noreply.github.com>
2021-05-11 19:24:12 +0300

    Update ReferalPage.js

--- pageobjects/Referal/ReferalPage.js
+++ pageobjects/Referal/ReferalPage.js
@@ -5,7 +5,7 @@ let BasePage = require('./../BasePage');
 class ReferalPage extends BasePage {
     constructor() {
         super();
-        this.appNameHeader += "/careers/refer-to-epam#form";
+        this.appNameHeader += "careers/refer-to-epam#form";
         this.submitFormButtom = element(by.class('button-ui-wrapper right-button button-submit'));
         this.successField = element(by.class("form-constructor-ui form-component form-component--standalone              has-error show-success"));
 
